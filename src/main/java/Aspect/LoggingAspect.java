19a39710b7d7c443004f4486ce69f12103993f6a
Tavi <taviatwork@gmail.com>
2020-11-02 02:37:30 +0200

    Finished aspects

--- src/main/java/Aspect/LoggingAspect.java
+++ src/main/java/Aspect/LoggingAspect.java
@@ -6,15 +6,43 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
 @Aspect
 @Component
 public class LoggingAspect {
 
     @Around("execution(* service.Library.addBook(..))")
-    public void logMessage(ProceedingJoinPoint jointPoint) throws Throwable{
-        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAA");
-        jointPoint.proceed();
-        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAA");
-        System.out.println(jointPoint.getSignature());
+    public void logMessage(ProceedingJoinPoint joinPoint) throws Throwable{
+        System.out.println("The method: " + joinPoint.getSignature().getName() +
+                " from " + joinPoint.getTarget().getClass()
+                + " will be executed, with the following parameters: " +
+                Arrays.toString(joinPoint.getArgs()) + ". Timestamp: " +
+                LocalDateTime.now());
+
+        joinPoint.proceed();
+
+        System.out.println("Method " + joinPoint.getSignature().getName() +
+                " from " + joinPoint.getTarget().getClass() +
+                " finished the execution. Timestamp: " +
+                LocalDateTime.now() + "\n");
+        System.out.println("A new book was added");
     }
+
+    @Around("execution(* service.Library.returnBookById(..))")
+    public void logMessageReturnedBook(ProceedingJoinPoint joinPoint) throws Throwable{
+        String id = Arrays.toString(joinPoint.getArgs());
+        joinPoint.proceed();
+        System.out.println("The book with id: " + id + " was returned at "+ LocalDateTime.now());
+    }
+
+    @Around("execution(* service.Library.borrow(..))")
+    public void logMessageBorrowedBook(ProceedingJoinPoint joinPoint) throws Throwable{
+        String args = Arrays.toString(joinPoint.getArgs());
+        String[] moreArgs = args.split(",",2);
+        joinPoint.proceed();
+        System.out.println("The book with id: " + moreArgs[0] + " was borrowed at "+ LocalDateTime.now() + " by " + moreArgs[1]);
+    }
+
 }
